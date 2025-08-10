91b75550cc3b086312e9ddb64ff8d5fdb0fa008d
steffil13 <ashinsteffil13@gmail.com>
2025-08-10 16:48:44 +0530

    Incomplete phamacist

--- src/app/pharmacists/pharmacists.module.ts
+++ src/app/pharmacists/pharmacists.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
 
-import { PharmacistsRoutingModule } from './pharmacists-routing.module';
-import { PharmacistComponent } from './pharmacist.component';
-import { FormsModule } from '@angular/forms';
+// import { PharmacistsRoutingModule } from './pharmacists-routing.module';
+// import { PharmacistComponent } from './pharmacist.component';
+// import { FormsModule } from '@angular/forms';
 
 
-@NgModule({
-  declarations: [
-    PharmacistComponent
-  ],
-  imports: [
-    CommonModule,
-    PharmacistsRoutingModule,
-    FormsModule 
-  ]
-})
-export class PharmacistsModule { }
+// @NgModule({
+//   declarations: [
+//     PharmacistComponent
+//   ],
+//   imports: [
+//     CommonModule,
+//     PharmacistsRoutingModule,
+//     FormsModule 
+//   ]
+// })
+// export class PharmacistsModule { }
--- src/app/shared/Service/receptionist.service.ts
+++ src/app/shared/Service/receptionist.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Patient } from '../models/patient.model';
+import { Appointment } from '../models/appointment.model';
+import { Billing } from '../models/billing.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReceptionistService {
 
-  constructor() { }
+  private baseUrl = 'https://localhost:7288/api/Receptionist'; // Your backend URL
+
+  constructor(private http: HttpClient) {}
+
+  // 🧾 Patients
+  getAllPatients(): Observable<Patient[]> {
+    return this.http.get<Patient[]>(`${this.baseUrl}/GetAllPatients`);
+  }
+
+  getPatientById(id: number): Observable<Patient> {
+    return this.http.get<Patient>(`${this.baseUrl}/GetPatientById/${id}`);
+  }
+
+  addPatient(patient: Patient): Observable<any> {
+    return this.http.post(`${this.baseUrl}/AddPatient`, patient);
+  }
+
+  updatePatient(id: number, patient: Patient): Observable<any> {
+    return this.http.put(`${this.baseUrl}/UpdatePatient/${id}`, patient);
+  }
+
+  deletePatient(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/DeletePatient/${id}`);
+  }
+
+  // 📅 Appointments
+  getAppointments(): Observable<Appointment[]> {
+    return this.http.get<Appointment[]>(`${this.baseUrl}/GetAllAppointments`);
+  }
+
+  scheduleAppointment(appointment: Appointment): Observable<any> {
+    return this.http.post(`${this.baseUrl}/ScheduleAppointment`, appointment);
+  }
+
+  updateAppointment(id: number, appointment: Appointment): Observable<any> {
+    return this.http.put(`${this.baseUrl}/UpdateAppointment/${id}`, appointment);
+  }
+
+  deleteAppointment(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/DeleteAppointment/${id}`);
+  }
+
+  // 💳 Billing
+  getBills(): Observable<Billing[]> {
+    return this.http.get<Billing[]>(`${this.baseUrl}/GetAllBills`);
+  }
+
+  generateBill(bill: Billing): Observable<any> {
+    return this.http.post(`${this.baseUrl}/GenerateBill`, bill);
+  }
 }
